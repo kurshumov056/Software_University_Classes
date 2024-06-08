43a0a152ae5b22710faa9e6180825612f0441806
kurshumov056 <85812544+kurshumov056@users.noreply.github.com>
2024-06-07 23:44:41 -0500

    Add files via upload

---
+++ OOP - Software University/Classes & Objects/Class_&_Objects_Problem5/main.py
@@ -0,0 +1,18 @@
+from Project.Task import Task
+from Project.Section import Section
+
+task = Task("Make bed", "27/05/2020")
+print(task.change_name("Go to University"))
+print(task.change_due_date("28.05.2020"))
+task.add_comment("Don't forget laptop")
+print(task.edit_comment(0, "Don't forget laptop and notebook"))
+print(task.details())
+
+print('        =================================          ')
+section = Section("Daily tasks")
+print(section.add_task(task))
+second_task = Task("Make bed", "27/05/2020")
+section.add_task(second_task)
+
+print(section.clean_section())
+print(section.view_section())
