4a7fdcbc3e034c23ee4ac12e9bdaad8a3cea8d3c
kurshumov056 <85812544+kurshumov056@users.noreply.github.com>
2024-06-11 19:01:15 -0500

    Add files via upload

---
+++ OOP - Software University/Module3_Inheritance/Problem2/Bear.py
@@ -0,0 +1,7 @@
+from Project.Mammal import Mammal
+
+class Bear(Mammal):
+
+    def __init__(self, name):
+        super().__init__(name)
+
---
+++ OOP - Software University/Module3_Inheritance/Problem2/Gorilla.py
@@ -0,0 +1,6 @@
+
+from Project.Mammal import Mammal
+
+class Gorilla(Mammal):
+    def __init__(self, name):
+        super().__init__(name)
