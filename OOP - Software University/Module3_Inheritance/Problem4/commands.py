1b9b7ba74efa7689a926cefbc0c9adc93d20b151
kurshumov056 <85812544+kurshumov056@users.noreply.github.com>
2024-06-12 16:32:50 -0500

    Add files via upload

---
+++ OOP - Software University/Module3_Inheritance/Problem4/commands.py
@@ -0,0 +1,15 @@
+
+
+from Project.Car import Car
+from Project.Vehicle import Vehicle
+from Project.FamilyCar import FamilyCar
+
+vehicle = Vehicle(50, 150)
+print(Vehicle.DEFAULT_FUEL_CONSUMPTION)
+print(FamilyCar.DEFAULT_FUEL_CONSUMPTION)
+print(vehicle.fuel)
+print(vehicle.horse_power)
+print(vehicle.fuel_consumption)
+vehicle.drive(100)
+print(vehicle.fuel)
+family_car = FamilyCar(150, 150)
