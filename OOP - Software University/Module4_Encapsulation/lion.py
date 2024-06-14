0d48e28cb20bc72826f2ce401af8340ffa31143e
kurshumov056 <85812544+kurshumov056@users.noreply.github.com>
2024-06-13 22:49:41 -0500

    Add files via upload

---
+++ OOP - Software University/Module4_Encapsulation/lion.py
@@ -0,0 +1,9 @@
+
+from Project.animal import Animal
+
+class Lion(Animal)
+
+    def __self__(self, name: str, gender: str, age: int ):
+        
+        self.money_for_care = 50
+        super().__init__(name, gender, age, 50)
