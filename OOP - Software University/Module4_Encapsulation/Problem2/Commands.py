be723d79e0667ada5198a7068f39c2427c4a2f86
kurshumov056 <85812544+kurshumov056@users.noreply.github.com>
2024-06-16 10:39:59 -0500

    Update Commands.py

--- OOP - Software University/Module4_Encapsulation/Problem2/Commands.py
+++ OOP - Software University/Module4_Encapsulation/Problem2/Commands.py
@@ -3,6 +3,40 @@
 
 
 from Project.topping import Topping
+from Project.dough import Dough
+from Project.pizza import Pizza
 
-sweet_A = Topping('', 2)
+tomato_topping =Topping("tomato", 60)
+print(tomato_topping.topping_type)
+print(tomato_topping.weight)
 
+mushrooms_topping =  Topping("Mushrooms", 75)
+print(mushrooms_topping.topping_type)
+print(mushrooms_topping.weight)
+
+mozzarella_topping = Topping("Mozzarella", 80)
+print(mozzarella_topping.topping_type)
+print(mozzarella_topping.weight)
+
+
+cheddar_topping = Topping("Cheddar", 150)
+pepperoni_topping = Topping("Pepperoni", 120)
+white_flour_dough = Dough("White Flour", "Mixing", 200)
+print(white_flour_dough.flour_type)
+print(white_flour_dough.weight)
+print(white_flour_dough.baking_technique)
+
+whole_wheat_dough = Dough("Whole Wheat Flour", "Mixing", 200)
+print(whole_wheat_dough.weight)
+print(whole_wheat_dough.flour_type)
+print(whole_wheat_dough.baking_technique)
+
+p = Pizza("Margherita", whole_wheat_dough, 2)
+tomato_topping =Topping("tomato", 60)
+p.add_topping(tomato_topping)
+print(p.calculate_total_weight())
+
+p.add_topping(mozzarella_topping)
+print(p.calculate_total_weight())
+
+p.add_topping(mozzarella_topping)
